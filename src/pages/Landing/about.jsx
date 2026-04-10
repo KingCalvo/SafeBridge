0790cffea22b5bf1a8a7a6c76f0f33a2a52b26c6
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2026-04-10 17:55:10 -0600

    Nueva imagen de portada

--- src/pages/Landing/about.jsx
+++ src/pages/Landing/about.jsx
@@ -4,7 +4,7 @@ import Footer from "../../components/Footer";
 import { useScrollAnimation } from "../../hooks/useScrollAnimation";
 import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
 import PageTitle from "../../components/PageTitle";
-import SafeFondo from "../../assets/BridgeFondo.png";
+import SafeFondo from "../../assets/SafeBridge_Portada.png";
 
 const About = () => {
   useScrollAnimation();
